ef115a2b12cacf83ccca528d260df209ef0185f0
Domenico Nappo <domenico.nappo@gmail.com>
2020-02-26 15:48:04 +0100

    simplify setup

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ import sys
 from shutil import rmtree
 
 from setuptools import setup, find_packages, Command
-from nine import IS_PYTHON2
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 readme_file = os.path.join(current_dir, 'README.md')
@@ -50,7 +49,6 @@ with open(readme_file, 'r') as f:
 with open(version_file, 'r') as f:
     version = f.read()
 
-numpy_version = '1.17.2' if not IS_PYTHON2 else '1.15.4'
 
 class UploadCommand(Command):
     """Support setup.py upload."""
@@ -101,9 +99,9 @@ setup_args = dict(
     long_description_content_type='text/markdown',
     setup_requires=[
             'setuptools>=41.0', 'nine',
-            'numpy=={}'.format(numpy_version),
+            'numpy<=1.17.2',
     ],
-    install_requires=['numpy=={}'.format(numpy_version), 'pyyaml==5.3', 'netCDF4==1.5.3', 'xarray==0.15.0',
+    install_requires=['numpy<=1.17.2', 'pyyaml==5.3', 'netCDF4==1.5.3', 'xarray==0.15.0',
                       'dask==2.7.0', 'pandas==0.25.1', 'pathlib2==2.3.5', 'nine'],
     author="Valerio Lorini, Domenico Nappo, Lorenzo Alfieri",
     author_email="valerio.lorini@ec.europa.eu,domenico.nappo@gmail.com,lorenzo.alfieri@ec.europa.eu",
