da32ce5aa7407a61bad3b8cec7ee0d0465ca31d5
Domenico Nappo <domenico.nappo@gmail.com>
2020-10-15 22:44:59 +0200

    add timestep comparison for tss

--- src/lisfloodutilities/compare/__init__.py
+++ src/lisfloodutilities/compare/__init__.py
@@ -100,6 +100,25 @@ class PCRComparator(Comparator):
             assert True
 
 
+def find_timestep(tss_file, timestep):
+    found_timestep = False
+    current_line = tss_file.readline()
+    while not found_timestep:
+        if not current_line:
+            break
+        try:
+            current_timestep = current_line.strip().split()[0]
+        except IndexError:
+            current_line = tss_file.readline()
+        else:
+            if current_timestep.decode() == str(timestep):
+                found_timestep = True
+                break
+            else:
+                current_line = tss_file.readline()
+    return current_line, found_timestep
+
+
 class TSSComparator(Comparator):
     glob_expr = ['**/*.tss']
 
@@ -110,15 +129,28 @@ class TSSComparator(Comparator):
         self.atol = atol
         self.rtol = rtol
 
-    def compare_lines_equal(self, file_a, file_b):
-        with open(file_a, 'rb') as fp1, open(file_b, 'rb') as fp2:
-            # skip first line in TSS as it just reports settings filename
-            fp1.readline()
-            fp2.readline()
+    def _findline_at_timestep(self, tss_file, timestep):
+        b1, found_timestep = find_timestep(tss_file, timestep)
+        if not found_timestep:
+            message = '{} not found in {}'.format(timestep, tss_file.name)
+            if self.for_testing:
+                assert False, message
+            else:
+                self.errors.append(message)
+        return b1
 
+    def compare_lines_equal(self, file_a, file_b, timestep=None):
+        tss1 = open(file_a, 'rb')
+        tss2 = open(file_b, 'rb')
+        # skip first line in TSS as it just reports settings filename
+        tss1.readline()
+        tss2.readline()
+
+        if not timestep:
+            # identical TSS files
             while True:
-                b1 = fp1.readline()
-                b2 = fp2.readline()
+                b1 = tss1.readline()
+                b2 = tss2.readline()
                 if (b1 != b2) or (not b1 and b2) or (not b2 and b1):
                     message = '{} different from {}'.format(file_a, file_b)
                     if self.for_testing:
@@ -128,42 +160,70 @@ class TSSComparator(Comparator):
                         return message
                 if not b1:
                     break
+        else:
+            # check line at a given timestep
+            b1 = self._findline_at_timestep(tss1, timestep)
+            b2 = self._findline_at_timestep(tss2, timestep)
 
-    def compare_lines_tolerance(self, file_a, file_b):
-        fp1 = open(file_a)
-        fp2 = open(file_b)
-        # skip first lines in TSS as it just reports settings filename
-        line = ''
-        numline = 1
-        while 'timestep' not in line:
-            fp1.readline()
-            line = fp2.readline()
-            numline += 1
-
-        while True:
-            b1 = fp1.readline().strip().split()
-            b2 = fp2.readline().strip().split()
-            if not b1:
-                break
-            array1 = np.array(b1, dtype='float64')
-            array2 = np.array(b2, dtype='float64')
-            if not np.allclose(array1, array2, rtol=self.rtol, atol=self.atol, equal_nan=True):
-                message = '{} different from {} (line: {})\n line A: {}\n line B: {}'.format(file_a, file_b, numline, array1, array2)
-                message += '\n {}'.format(np.abs(array1 - array2))
+            if b1 != b2:
+                message = '{} different from {} for timestep {}'.format(file_a, file_b, timestep)
                 if self.for_testing:
-                    np.testing.assert_allclose(array1, array2, rtol=self.rtol, atol=self.atol, equal_nan=True)
                     assert False, message
                 else:
                     self.errors.append(message)
         assert True
         return self.errors
 
+    def compare_lines_tolerance(self, file_a, file_b, timestep=None):
+        tss1 = open(file_a, 'rb')
+        tss2 = open(file_b, 'rb')
+        # skip first lines in TSS as it just reports settings filename
+        line = b''
+        numline = 1
+        while 'timestep' not in line.decode():
+            tss1.readline()
+            line = tss2.readline()
+            numline += 1
+
+        while True:
+            if not timestep:
+                b1 = tss1.readline().strip().split()
+                b2 = tss2.readline().strip().split()
+                if not b1:
+                    break
+                self._check_tss_line_tol(b1, b2, file_a, file_b, numline)
+            else:
+                b1 = self._findline_at_timestep(tss1, timestep)
+                b2 = self._findline_at_timestep(tss2, timestep)
+
+                b1 = b1.strip().split()
+                b2 = b2.strip().split()
+                self._check_tss_line_tol(b1, b2, file_a, file_b, numline)
+                # just one line
+                break
+
+        assert True
+        return self.errors
+
+    def _check_tss_line_tol(self, b1, b2, file_a, file_b, numline):
+        array1 = np.array(b1, dtype='float64')
+        array2 = np.array(b2, dtype='float64')
+        if not np.allclose(array1, array2, rtol=self.rtol, atol=self.atol, equal_nan=True):
+            message = '{} different from {} (line: {})\n line A: {}\n line B: {}'.format(file_a, file_b, numline,
+                                                                                         array1, array2)
+            message += '\n {}'.format(np.abs(array1 - array2))
+            if self.for_testing:
+                np.testing.assert_allclose(array1, array2, rtol=self.rtol, atol=self.atol, equal_nan=True)
+                assert False, message
+            else:
+                self.errors.append(message)
+
     def compare_files(self, file_a, file_b, timestep=None):
         logger.info('Comparing %s and %s', file_a, file_b)
         if self.array_equal:
-            self.compare_lines_equal(file_a, file_b)
+            self.compare_lines_equal(file_a, file_b, timestep)
         else:
-            self.compare_lines_tolerance(file_a, file_b)
+            self.compare_lines_tolerance(file_a, file_b, timestep)
 
 
 class NetCDFComparator(Comparator):
--- tests/test_compare.py
+++ tests/test_compare.py
@@ -27,14 +27,14 @@ class TestComparators:
         comp.compare_files('tests/data/folder_a/ta.nc', 'tests/data/folder_b/ta.nc')
         with pytest.raises(AssertionError) as excinfo:
             comp.compare_files('tests/data/folder_a/ta.nc', 'tests/data/folder_a/tp.nc')
-            assert 'have different variables names' in excinfo.value
+        assert 'have different variables names' in str(excinfo.value)
 
     def test_netcdfcomp_dirs(self):
         comp = NetCDFComparator('tests/data/area.nc', for_testing=True)
         comp.compare_dirs('tests/data/folder_a/', 'tests/data/folder_b/')
         with pytest.raises(AssertionError) as excinfo:
             comp.compare_dirs('tests/data/folder_a/', 'tests/data/folder_b/', skip_missing=False)
-            assert 'is missing in tests/data/folder_b/' in excinfo.value
+        assert 'is missing in tests/data/folder_b' in str(excinfo.value)
 
     def test_netcdfcomp_tol(self):
         comp = NetCDFComparator('tests/data/folder_d/mask.nc', atol=0.05, rtol=0.1, for_testing=True)
@@ -44,49 +44,67 @@ class TestComparators:
         with pytest.raises(AssertionError) as excinfo:
             comp = NetCDFComparator('tests/data/folder_d/mask.nc', atol=0.0005, rtol=0.001, for_testing=True)
             comp.compare_files('tests/data/folder_d/a.nc', 'tests/data/folder_d/b.nc')
-            assert 'of different values - max diff:' in excinfo.value
+        assert 'of different values - max diff:' in str(excinfo.value)
 
     def test_netcdfcomp_identical(self):
         comp = NetCDFComparator('tests/data/folder_d/mask.nc', array_equal=True, for_testing=True)
         comp.compare_files('tests/data/folder_a/ta.nc', 'tests/data/folder_b/ta.nc')
         with pytest.raises(AssertionError) as excinfo:
             comp.compare_files('tests/data/folder_d/a.nc', 'tests/data/folder_d/b.nc')
-            assert 'Arrays are not equal' in excinfo.value
+        assert 'Arrays are not equal' in str(excinfo.value)
 
     def test_tss(self):
-        comp = TSSComparator(for_testing=True, array_equal=True)
-        comp.compare_files('tests/data/folder_a/qLakeIn.tss', 'tests/data/folder_b/qLakeIn.tss')
         with pytest.raises(AssertionError) as excinfo:
+            comp = TSSComparator(array_equal=True)
             comp.compare_files('tests/data/folder_a/qLakeOut.tss', 'tests/data/folder_b/qLakeOut.tss')
-            assert 'tests/data/folder_a/qLakeOut.tss different from tests/data/folder_b/qLakeOut.tss' in excinfo.value
+
+        assert 'tests/data/folder_a/qLakeOut.tss different from tests/data/folder_b/qLakeOut.tss' in str(excinfo.value)
+        comp.compare_files('tests/data/folder_a/qLakeIn.tss', 'tests/data/folder_b/qLakeIn.tss')
+
         with pytest.raises(AssertionError) as excinfo:
             comp.compare_dirs('tests/data/folder_a/', 'tests/data/folder_b/')
-            assert 'tests/data/folder_a/qLakeOut.tss different from tests/data/folder_b/qLakeOut.tss' in excinfo.value
+        assert 'tests/data/folder_a/qLakeOut.tss different from tests/data/folder_b/qLakeOut.tss' in str(excinfo.value)
+
+    def test_tss_timestep(self):
+        comp = TSSComparator(for_testing=True, array_equal=True)
+        timestep = 62
+        comp.compare_files('tests/data/folder_a/qLakeIn.tss', 'tests/data/folder_b/qLakeInSingleTimestep.tss', timestep)
+        with pytest.raises(AssertionError) as excinfo:
+            comp.compare_files('tests/data/folder_b/qLakeOut.tss', 'tests/data/folder_b/qLakeOut2.tss', timestep)
+        assert '62 not found in tests/data/folder_b/qLakeOut2.tss' in str(excinfo.value)
+
+    def test_tss_timestep_tol(self):
+        comp = TSSComparator(for_testing=True, rtol=0.00001, atol=0.000001)
+        timestep = 62
+        comp.compare_files('tests/data/folder_b/qLakeIn.tss', 'tests/data/folder_b/qLakeInSingleTimestep.tss', timestep)
+        with pytest.raises(AssertionError) as excinfo:
+            comp.compare_files('tests/data/folder_b/qLakeOut.tss', 'tests/data/folder_b/qLakeOut3.tss', timestep)
+        assert 'Not equal to tolerance rtol=1e-05, atol=1e-06\n\nMismatched elements: 1 / 2 (50%)' in str(excinfo.value)
 
     def test_tss_tolerance(self):
         comp = TSSComparator(for_testing=True, rtol=0.001, atol=0.0001)
         comp.compare_files('tests/data/folder_a/test_tol_ok_1.tss', 'tests/data/folder_b/test_tol_ok_2.tss')
         with pytest.raises(AssertionError) as excinfo:
             comp.compare_files('tests/data/folder_a/test_tol_fail_1.tss', 'tests/data/folder_b/test_tol_fail_2.tss')
-            assert 'tests/data/folder_a/test_tol_fail_1.tss different from tests/data/folder_b/test_tol_fail_2.tss (line: 4)' in excinfo.value
+        assert 'Not equal to tolerance rtol=0.001, atol=0.0001\n\nMismatched elements: 1 / 2 (50%)\nMax absolute difference: 0.007' in str(excinfo.value)
         comp = TSSComparator(for_testing=True, rtol=0.00001, atol=0.000001)
         with pytest.raises(AssertionError) as excinfo:
             comp.compare_files('tests/data/folder_a/test_tol_ok_1.tss', 'tests/data/folder_b/test_tol_ok_2.tss')
-            assert 'tests/data/folder_a/test_tol_ok_1.tss different from tests/data/folder_b/test_tol_ok_2.tss (line: 4)' in excinfo.value
+        assert 'Not equal to tolerance rtol=1e-05, atol=1e-06\n\nMismatched elements: 1 / 2 (50%)\nMax absolute difference: 5.e-05' in str(excinfo.value)
 
     def test_pcr(self):
         comp = PCRComparator(for_testing=True)
         with pytest.raises(AssertionError) as excinfo:
             comp.compare_files('tests/data/folder_b/1.map', 'tests/data/folder_c/1.map')
-            assert 'tests/data/folder_b/1.map different from tests/data/folder_c/1.map' in excinfo.value
+        assert 'tests/data/folder_b/1.map different from tests/data/folder_c/1.map' in str(excinfo.value)
         comp.compare_files('tests/data/folder_a/1.map', 'tests/data/folder_c/1.map')
         with pytest.raises(AssertionError) as excinfo:
             comp.compare_dirs('tests/data/folder_a/', 'tests/data/folder_b/')
-            assert 'tests/data/folder_b/1.map different from tests/data/folder_c/1.map' in excinfo.value
+        assert 'tests/data/folder_a/1.map different from tests/data/folder_b/1.map' in str(excinfo.value)
         comp.compare_dirs('tests/data/folder_a/', 'tests/data/folder_c/')
         with pytest.raises(AssertionError) as excinfo:
             comp.compare_dirs('tests/data/folder_a/', 'tests/data/folder_c/', skip_missing=False)
-            assert '.map is missing in tests/data/folder_c' in excinfo.value
+        assert '.map is missing in tests/data/folder_c' in str(excinfo.value)
 
     def test_comp_no_asserts(self):
         comp = NetCDFComparator('tests/data/folder_d/mask.nc', array_equal=True, for_testing=False)
