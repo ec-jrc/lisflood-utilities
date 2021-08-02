84981e2b93d94db7b703b05178ab24cb108d2a09
unknown <hylke.beck@gmail.com>
2021-08-02 21:27:43 +0200

    Snapping is working now

--- snap_locations.py
+++ snap_locations.py
@@ -17,12 +17,12 @@ import matplotlib.pyplot as plt
 def latlon2rowcol(lat,lon,res,lat_upper,lon_left):
     row = np.round((lat_upper-lat)/res-0.5).astype(int)
     col = np.round((lon-lon_left)/res-0.5).astype(int)    
-    return row,col
+    return row.squeeze(),col.squeeze()
 
 def rowcol2latlon(row,col,res,lat_upper,lon_left):
     lat = lat_upper-row*res-res/2
     lon = lon_left+col*res+res/2
-    return lat,lon
+    return lat.squeeze(),lon.squeeze()
     
 def readmatfile(filepath,var):
     try:
@@ -54,6 +54,12 @@ ldd_np = ldd_np.astype(np.int16)
 lat = np.array(dset.variables['lat'][:])
 lon = np.array(dset.variables['lon'][:])
 res = np.diff(lon)[0]
+lat_upper = lat[0]+res/2
+lon_left = lon[0]-res/2
+
+# ##### LOAD UPS MAP
+dset = Dataset(r"//zeus/hylkeb/Temp/ups.nc")
+ups_np = np.array(dset.variables['ups'][:])
 
 # Set pcraster clone map
 pcr.setclone(ldd_np.shape[0],ldd_np.shape[1],res,lon[0]-res/2,lat[0]-res/2)
@@ -69,120 +75,147 @@ upstreamarea_pcr = pcr.accuflux(ldd_pcr,area_pcr)
 upstreamarea_np = pcr.pcr2numpy(upstreamarea_pcr,mv=-9999)
 
 # Initialize qgis csv
-df = pd.DataFrame(columns = ["ObsID","StationName","Provider ID","Country code","StationLat","StationLon","Height","Height Units","DrainingArea.km2.Provider","Catchment Area Units","Added Date","River","Catchment","EC_Catchments","Calibration ID","DrainingArea.km2.LDD","LisfloodX","LisfloodY","RealTime","PostProcess","FixedRepPoint","HasWL","HasD","Alive D","StartDate_hist_24","EndDate_hist_24","StartDate_nrt_24","EndDate_nrt_24","StartDate_hist_6","EndDate_hist_6","StartDate_nrt_6","EndDate_nrt_6","DiffDays_hist_24","DiffDays_nrt_24","DiffDays_hist_6","DiffDays_nrt_6","cal_hist_24h","cal_nrt_24h","cal_hist_6h","cal_nrt_6h","CAL_TYPE","Notes","EC_calib","Dam/Lake","EnoughQdata","Val_Start","Val_End","Cal_Start","Cal_End","CatchmentArea","SamplingFrequency"])
+df = pd.DataFrame(columns = ["ObsID","StationName","Provider ID","Country code","StataaaaLat","StataaaaLon","Height","Height Units","DrainingArea.km2.Provider","Catchment Area Units","Added Date","River","Catchment","EC_Catchments","Calibration ID","DrainingArea.km2.LDD","LisfloodX","LisfloodY","RealTime","PostProcess","FixedRepPoint","HasWL","HasD","Alive D","StartDate_hist_24","EndDate_hist_24","StartDate_nrt_24","EndDate_nrt_24","StartDate_hist_6","EndDate_hist_6","StartDate_nrt_6","EndDate_nrt_6","DiffDays_hist_24","DiffDays_nrt_24","DiffDays_hist_6","DiffDays_nrt_6","cal_hist_24h","cal_nrt_24h","cal_hist_6h","cal_nrt_6h","CAL_TYPE","Notes","EC_calib","Dam/Lake","EnoughQdata","Val_Start","Val_End","Cal_Start","Cal_End","CatchmentArea","SamplingFrequency"])
 
 # Loop over catchments and snap station locations to the 'correct' grid cell
 catchment_dirs = glob.glob(os.path.join(databasedir,'*',""))
 catchment_dirs = sorted(catchment_dirs)
 for ii in np.arange(len(catchment_dirs)):
-    catchment_dir = catchment_dirs[ii]    
+    if ii!=421: continue
+    catchment_dir = catchment_dirs[ii]  
+    print("===============================================================================")
     print(str(ii)+" "+os.path.split(os.path.dirname(catchment_dir))[-1])    
     t1 = time.time()
     done = False
-   
+    toosmall = False
+    snapped = False
+    
+    if 'Discharge' in globals(): del Discharge
+    if 'StatLat' in globals(): del StatLat
+    if 'StatLon' in globals(): del StatLon
+    if 'Area' in globals(): del Area
+    if 'CatchCentroidLat' in globals(): del CatchCentroidLat
+    if 'CatchCentroidLon' in globals(): del CatchCentroidLon
+
+    
+    ############################################################################
+    #   Load discharge and catchment boundary data
+    ############################################################################
+    
     # Load discharge data
     try:
         Discharge = readmatfile(os.path.join(catchment_dir,"DISCHARGE.mat"),'DISCHARGE/Discharge')
-        StationCoordsLat = readmatfile(os.path.join(catchment_dir,"DISCHARGE.mat"),'DISCHARGE/StationCoords/Lat')[0]
-        StationCoordsLon = readmatfile(os.path.join(catchment_dir,"DISCHARGE.mat"),'DISCHARGE/StationCoords/Lon')[0]
+        StatLat = readmatfile(os.path.join(catchment_dir,"DISCHARGE.mat"),'DISCHARGE/StationCoords/Lat')[0][0]
+        StatLon = readmatfile(os.path.join(catchment_dir,"DISCHARGE.mat"),'DISCHARGE/StationCoords/Lon')[0][0]
+        StatRow,StatCol = latlon2rowcol(StatLat,StatLon,res,lat_upper,lon_left)
     except:
         print("Unable to load DISCHARGE.mat, skipping")
         continue
         
     # Load catchment boundaries    
-    if 'Area' in globals(): del Area
     try:
-        Area = np.float(readmatfile(os.path.join(catchment_dir,"BOUNDARIES.mat"),'BOUNDARIES/Area'))
-        
-        '''
-        CatchBoundsLat = readmatfile(os.path.join(catchment_dir,"BOUNDARIES.mat"),'BOUNDARIES/CatchBounds/Lat')
-        CatchBoundsLon = readmatfile(os.path.join(catchment_dir,"BOUNDARIES.mat"),'BOUNDARIES/CatchBounds/Lon')
-        
-        # Create catchment boundaries array
-        if len(CatchBoundsLat[0,:])!=1: CatchBoundsLat = CatchBoundsLat.transpose()
-        if len(CatchBoundsLon[0,:])!=1: CatchBoundsLon = CatchBoundsLon.transpose()    
-        CatchBounds = np.concatenate((CatchBoundsLon,CatchBoundsLat),axis=1)
-
-        # Plot for testing purposes
-        plt.plot(CatchBoundsLon,CatchBoundsLat,color='k',linewidth=0.5)
-        
-        # Create grid mesh
-        lattop = np.ceil(np.nanmax(CatchBoundsLat)/resolution)*resolution-resolution/2
-        lattop_ind = np.int(np.round((90-lattop)/resolution-0.5))
-        latbottom = np.floor(np.nanmin(CatchBoundsLat)/resolution)*resolution+resolution/2
-        latbottom_ind = np.int(np.round((90-latbottom)/resolution-0.5))+1
-        lonright = np.ceil(np.nanmax(CatchBoundsLon)/resolution)*resolution-resolution/2
-        lonright_ind = np.int(np.round((lonright+180)/resolution-0.5))+1
-        lonleft = np.floor(np.nanmin(CatchBoundsLon)/resolution)*resolution+resolution/2
-        lonleft_ind = np.int(np.round((lonleft+180)/resolution-0.5))
-        latsub = lat[lattop_ind:latbottom_ind]
-        lonsub = lon[lonleft_ind:lonright_ind]
-        x, y = np.meshgrid(lonsub, latsub)
-        x, y = x.flatten(), y.flatten()
-        point = np.vstack((x,y)).T 
-
-        # Make catchment mask
-        p = Path(CatchBounds) # Make polygon
-        grid = p.contains_point(point)
-        mask = grid.reshape(len(latsub),len(lonsub)) 
-        '''
-
+        Area = float(readmatfile(os.path.join(catchment_dir,"BOUNDARIES.mat"),'BOUNDARIES/Area'))        
+        CatchBoundsLat = readmatfile(os.path.join(catchment_dir,"BOUNDARIES.mat"),'BOUNDARIES/CatchBounds/Lat')[0]
+        CatchBoundsLon = readmatfile(os.path.join(catchment_dir,"BOUNDARIES.mat"),'BOUNDARIES/CatchBounds/Lon')[0]
+        CatchCentroidLat = (np.nanmin(CatchBoundsLat)+np.nanmax(CatchBoundsLat))/2
+        CatchCentroidLon = (np.nanmin(CatchBoundsLon)+np.nanmax(CatchBoundsLon))/2
     except:
         print("Unable to load BOUNDARIES.mat")
-        continue
+    
+        
+    ############################################################################
+    #   If we have area and catchment boundary estimates, we will automatically 
+    #   determine the most suitable grid cell
+    ############################################################################
+    
+    if 'Area' in globals():
+    
+        if Area<10000:             
+            print("Catchment too small")
+            continue
 
-    # Settings
-    search_margin = 1 # Grid cells
-    area_permissible_error = 20 # Percent        
-    
-    # Station row col (initial estimate)
-    row,col = latlon2rowcol(StationCoordsLat,StationCoordsLon,res,lat[0]+res/2,lon[0]-res/2)
-    
-    # If we have area estimate
-    if 'Area' in globals(): 
-        error_map = 100*np.abs(upstreamarea_np/Area-1)
-        mask = np.zeros(ldd_np.shape,dtype=np.bool)
-        mask[row-search_margin,row+search_margin+1,col-search_margin,col+search_margin+1] = True
-        error_map[~mask] = 9999
-        if error_map[row,col]<area_permissible_error: 
-            done = True
+        # Snap settings
+        permissible_area_error = 20 # Maximum allowed catchment area error (percent)
+        permissible_centroid_error = 0.5 # Maximum allowed centroid location error (degrees)
+        margin = 2 # Search box size (grid cells around station)
+        
+        # Compute area error
+        area_error_map = 100*np.abs(upstreamarea_np-Area)/Area
+        mask = np.zeros(ldd_np.shape,dtype=bool)
+        mask[StatRow-margin:StatRow+margin+1,StatCol-margin:StatCol+margin+1] = True
+        area_error_map[~mask] = 9999
+        
+        # Compute centroid error
+        centroid_error_map = np.zeros(ldd_np.shape,dtype=np.single)+9999
+        indices = zip(*np.where(mask))
+        for i, j in indices:
+            point_np = np.zeros(ldd_np.shape,dtype=bool)
+            point_np[i,j] = True
+            point_pcr = pcr.numpy2pcr(pcr.Boolean,point_np,mv=-9999)
+            catch_pcr = pcr.catchment(ldd_pcr, point_pcr)        
+            catch_np = pcr.pcr2numpy(catch_pcr,mv=-9999)
+            catch_np = catch_np==1
+            if np.sum(catch_np)==0: continue
+            ind = np.where(catch_np)
+            centroid_row = (ind[0][0]+ind[0][-1])/2
+            centroid_col = (ind[1][0]+ind[1][-1])/2
+            centroid_lat,centroid_lon = rowcol2latlon(centroid_row,centroid_col,res,lat_upper,lon_left)
+            centroid_error_map[i,j] = np.sqrt((centroid_lat-CatchCentroidLat)**2+(centroid_lon-CatchCentroidLon)**2)                       
+        
+        # Determine the most appropriate station grid cell
+        area_error = area_error_map[StatRow,StatCol]
+        centroid_error = centroid_error_map[StatRow,StatCol]
+        if (area_error<permissible_area_error) & (centroid_error<permissible_centroid_error):
+            StatRowCorr,StatColCorr = StatRow,StatCol
+            print('Station snapping not necessary')
         else:
-            error_map
-            np.where(error_map==np.min(error_map))
-            pdb.set_trace()
-            
-            
-        
-        
-    if done==False:
-    
-    
-    
-    upstreamarea_pcr
-    
-    
-    
-    for rr in np.arange(row-search_margin,row+search_margin+1):
-        for cc in np.arange(col-search_margin,col+search_margin+1):
-            
-    point_np = np.zeros(ldd_np.shape,dtype=np.bool)
-    point_np[row,col] = True
-    point_pcr = pcr.numpy2pcr(pcr.Boolean,point_np,mv=-9999)
-    catch_pcr = pcr.catchment(ldd_pcr, point_pcr)
-    
-    pcr.aguila(catch_pcr)
-    
+            rank_area_error_map = area_error_map.ravel().argsort().argsort().reshape(area_error_map.shape)
+            rank_centroid_error_map = centroid_error_map.ravel().argsort().argsort().reshape(centroid_error_map.shape)
+            rank_cum_map = rank_area_error_map+rank_centroid_error_map            
+            StatRowCorr,StatColCorr = np.where(rank_cum_map==np.min(rank_cum_map))
+            StatRowCorr,StatColCorr = StatRowCorr[0],StatColCorr[0]
+            StatLatCorr,StatLonCorr = rowcol2latlon(StatRowCorr,StatColCorr,res,lat_upper,lon_left)
+            print('Station snapped')
 
+        area_error = area_error_map[StatRowCorr,StatColCorr]
+        centroid_error = centroid_error_map[StatRowCorr,StatColCorr]    
+        print("Area error: "+str(area_error)+" %, centroid error: "+str(centroid_error)+" degrees")
+        
+        # Plot stuff
+        if (area_error>permissible_area_error) | (centroid_error>permissible_centroid_error):
+            point_np = np.zeros(ldd_np.shape,dtype=bool)
+            point_np[StatRowCorr,StatColCorr ] = True
+            point_pcr = pcr.numpy2pcr(pcr.Boolean,point_np,mv=-9999)
+            catch_pcr = pcr.catchment(ldd_pcr, point_pcr)        
+            catch_np = pcr.pcr2numpy(catch_pcr,mv=-9999)
+            catch_np = catch_np==1
+            plt.clf()
+            extent = [lon_left,lon_left+len(lon)*res,lat_upper-len(lat)*res,lat_upper] # (left, right, bottom, top)
+            plt.imshow(np.single(catch_np)+np.log10(upstreamarea_np)/4, extent=extent)
+            plt.plot(CatchBoundsLon,CatchBoundsLat)
+            plt.gca().set_xlim(np.nanmin(CatchBoundsLon)-6,np.nanmax(CatchBoundsLon)+6)
+            plt.gca().set_ylim(np.nanmin(CatchBoundsLat)-6,np.nanmax(CatchBoundsLat)+6)
+            plt.title(str(ii))
+            plt.show()
+        
+        
     pdb.set_trace()
+    
+    
+    ############################################################################
+    #   If we don't have area and catchment boundary estimates, we have to 
+    #   pick the most suitable grid cell manually
+    ############################################################################
+            
+    #if 'Area' not in globals():     
+    #    continue
+    
+    
+    ############################################################################
+    #   If we don't have area and catchment boundary estimates, we have to 
+    #   pick the most suitable grid cell manually
+    ############################################################################        
 
-
-pdb.set_trace()
-pcr.setclone(ldd)
-
-
-
-pcr.catchment(ldd,point)
-pdb.set_trace()
 
 
 '''
@@ -239,9 +272,9 @@ for ii in np.arange(len(ADHI_stations)):
     # Basic station info
     ObsID = str(ii)
     StationName = ADHI_stations['Name'][0]
-    StationLat = ADHI_stations['Latitude'][0]
-    StationLon = ADHI_stations['Longitude'][0]
-    df = df.append({'ObsID':ObsID, 'StationName':StationName, 'StationLat':StationLat,'StationLon':StationLon},ignore_index=True)
+    StatLat = ADHI_stations['Latitude'][0]
+    StatLon = ADHI_stations['Longitude'][0]
+    df = df.append({'ObsID':ObsID, 'StationName':StationName, 'StatLat':StatLat,'StatLon':StatLon},ignore_index=True)
     pdb.set_trace()
 
     # Catchmemt area and boundaries
