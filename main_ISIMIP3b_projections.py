8067b874e0da06c225ac3740ec1a185db4fd7383
Hylke Beck <hylke.beck@gmail.com>
2022-03-01 16:15:22 +0100

    Development

--- main_ISIMIP3b_projections.py
+++ main_ISIMIP3b_projections.py
@@ -9,11 +9,10 @@ import os, sys, glob, time, pdb
 import pandas as pd
 import numpy as np
 from netCDF4 import Dataset
-import matplotlib.pyplot as plt
 from tools import *
-import rasterio
+from skimage.transform import resize
 
-# Load configuration file
+# Load configuration file 
 config = load_config(sys.argv[1])
 
 
@@ -32,14 +31,94 @@ def main():
     mapsize_template = template_np.shape
     row_upper,col_left = latlon2rowcol(template_lat[0],template_lon[0],template_res,90,-180)
     
-    # Process the following scenarios and years
-    pdb.set_trace()
-    for scenario in scenarios:    
-        for model in models:
-            #years = 
-            for year in years:
-                years = np.arange(2020,2110,10)
-    
+    # Loop through scenarios and models
+    files = glob.glob(os.path.join(config['meteo_data_folder'],'*.nc'))
+    scenarios = np.unique([os.path.basename(x).split('_')[3] for x in files])
+    for scenario in scenarios:
+        files = glob.glob(os.path.join(config['meteo_data_folder'],'*'+scenario+'*.nc'))
+        sort(files)
+        models = np.unique([os.path.basename(x).split('_')[0] for x in files])
+        for model in models:        
+
+            # What is time span of output?
+            if scenario=='historical':
+                year_start = 1979
+                year_end = 2014
+            else:
+                year_start = 2015
+                year_end = 2100
+            out_dates_dly = pd.date_range(start=datetime(year_start,1,1), end=datetime(year_end+1,1,1)-pd.Timedelta(days=1), freq='D')
+            
+            # Initialize output netCDF
+            varname = 'pr'
+            varname_isimip3b = 'pr_' # 'tas_'
+            factor = 86400
+            offset = 0
+            units = 'mm/d'
+            if os.path.isdir(os.path.join(config['output_folder'],scenario,model))==False:
+                os.makedirs(os.path.join(config['output_folder'],scenario,model))
+            outfile = os.path.join(config['output_folder'],scenario,model,varname+'.nc')
+            precision = 1
+            if os.path.isfile(outfile):
+                os.remove(outfile)                
+            ncfile = Dataset(outfile, 'w', format='NETCDF4')
+            ncfile.history = 'Created on %s' % datetime.utcnow().strftime('%Y-%m-%d %H:%M')
+            ncfile.createDimension('lon', len(template_lon))
+            ncfile.createDimension('lat', len(template_lat))
+            ncfile.createDimension('time', None)
+            ncfile.createVariable('lon', 'f8', ('lon',))
+            ncfile.variables['lon'][:] = template_lon
+            ncfile.variables['lon'].units = 'degrees_east'
+            ncfile.variables['lon'].long_name = 'longitude'
+            ncfile.createVariable('lat', 'f8', ('lat',))
+            ncfile.variables['lat'][:] = template_lat
+            ncfile.variables['lat'].units = 'degrees_north'
+            ncfile.variables['lat'].long_name = 'latitude'
+            ncfile.createVariable('time', 'f8', 'time')
+            ncfile.variables['time'].units = 'days since 1979-01-02 00:00:00'
+            ncfile.variables['time'].long_name = 'time'
+            ncfile.variables['time'].calendar = 'proleptic_gregorian'
+            ncfile.createVariable(varname, np.single, ('time', 'lat', 'lon'), zlib=True, chunksizes=(1,450,450,), fill_value=-9999,least_significant_digit=precision)
+            ncfile.variables[varname].units = units
+
+            # Loop over input files
+            files = glob.glob(os.path.join(config['meteo_data_folder'],'*'+model+'*'+scenario+'*'+varname_isimip3b+'*.nc'))
+            for file in files:
+                file_year_start = int(os.path.basename(file).split('_')[7])
+                file_year_end = int(os.path.basename(file).split('_')[8][:-3])
+                count = np.sum((out_dates_dly.year>=file_year_start) & (out_dates_dly.year<=file_year_end))
+                if count==0:
+                    continue
+                
+                # Loop over days of input file
+                print('Processing '+file)
+                t0 = time.time()
+                file_dates_dly = pd.date_range(start=datetime(file_year_start,1,1), end=datetime(file_year_end+1,1,1)-pd.Timedelta(days=1), freq='D')                
+                dset = Dataset(file)
+                for ii in np.arange(len(file_dates_dly)):
+                    if file_dates_dly[ii] not in out_dates_dly:
+                        continue
+                    
+                    index = np.where(out_dates_dly==file_dates_dly[ii])[0][0]
+                    data = np.array(dset.variables['pr'][ii,:,:])
+                    data = factor*data+offset
+                    data = np.roll(data,360,axis=1)
+                    data = resize(data,mapsize_global,order=0,mode='edge',anti_aliasing=False)
+                    data = data[row_upper:row_upper+len(template_lat),col_left:col_left+len(template_lon)]
+                    
+                    # Write data to netCDF
+                    ncfile.variables['time'][index] = (file_dates_dly[ii]-pd.to_datetime(datetime(1979, 1, 1))).total_seconds()/86400
+                    ncfile.variables[varname][index,:,:] = data                    
+                
+                print("Time elapsed is "+str(time.time()-t0)+" sec")
+                   
+            ncfile.close()
+
+            pdb.set_trace()
+        
+            
+            
+            
     # Loop through scenarios
     for scenario in scenarios:
         
