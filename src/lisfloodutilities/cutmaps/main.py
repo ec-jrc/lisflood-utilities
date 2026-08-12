2466cf666377fa2af32c020c4fd3712cf53b8bbf
gnrgomes <ggomes76@gmail.com>
2026-08-12 12:37:07 +0200

    Change freeze timeout to 300s.

--- src/lisfloodutilities/cutmaps/main.py
+++ src/lisfloodutilities/cutmaps/main.py
@@ -48,7 +48,7 @@ from .helpers import COORDINATE_NAMES, TIME_NAMES
 # Maximum number of retry attempts for a frozen file before giving up
 MAX_RETRIES = 3
 # Timeout in seconds: if output file doesn't change for this duration, consider processing frozen
-FREEZE_TIMEOUT_SECONDS = 120
+FREEZE_TIMEOUT_SECONDS = 300
 
 # Variables that should be excluded from mask processing.
 # Common NetCDF projection/CRS variable names
